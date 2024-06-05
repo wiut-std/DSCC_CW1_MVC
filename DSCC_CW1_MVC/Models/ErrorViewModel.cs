d7279956571ba0f3b3f6ff15b4843a612ee74617
wiut-std <jjumaniyozov@students.wiut.uz>
2024-06-05 21:10:05 +0500

    Project creation

---
+++ DSCC_CW1_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DSCC_CW1_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
